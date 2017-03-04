4e46f8d570fc870fdf528256e517a0e8ed8c4255
godbiao <ahwangtingbiao@vip.qq.com>
2017-03-04 13:24:13 +0800

    baiduver

--- skins/js/common.js
+++ skins/js/common.js
@@ -230,3 +230,17 @@ function getsid(sum, num) {
 	a.length = num;
 	return a;
 }
+
+
+//自动推送百度搜索
+(function () {
+	var bp = document.createElement('script');
+	var curProtocol = window.location.protocol.split(':')[0];
+	if (curProtocol === 'https') {
+		bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+	} else {
+		bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+	}
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(bp, s);
+})();
