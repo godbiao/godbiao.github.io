8b2792444d517c51ea7db55d92bd0ff2561dc8ae
godbiao <ahwangtingbiao@vip.qq.com>
2017-04-26 10:57:13 +0800

    ajax

--- skins/js/skins.js
+++ skins/js/skins.js
@@ -1,7 +1,13 @@
 //异步加载皮肤表
+
+//加载JSON皮肤
 var skinsAjaxBackup = $.ajax({
     url: "skins.json",
     async: false
 });
+
+//获取皮肤文件内容
 var skinsData = skinsAjaxBackup.responseText;
+
+//转换为对象
 var skins = eval("(" + skinsData + ")");
