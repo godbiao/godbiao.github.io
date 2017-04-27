e7b13c6be49fbe0f12958d6cee6797c4f9ce76df
godbiao <ahwangtingbiao@vip.qq.com>
2017-04-27 13:12:39 +0800

    skins

--- skins/js/skins.js
+++ skins/js/skins.js
@@ -1,13 +1,18 @@
 //异步加载皮肤表
 
-//加载JSON皮肤
-var skinsAjaxBackup = $.ajax({
+//JQ加载JSON皮肤
+/*var skinsAjax = $.ajax({
     url: "https://godbiao.github.io/skins/skins.json",
     async: false
 });
+*/
+//原生加载JSON皮肤
+var skinsAjax = new XMLHttpRequest();
+skinsAjax.open('GET', 'https://godbiao.github.io/skins/skins.json', false);
+skinsAjax.send();
 
 //获取皮肤文件内容
-var skinsData = skinsAjaxBackup.responseText;
+var skinsData = skinsAjax.responseText;
 
 //转换为对象
 var skins = eval("(" + skinsData + ")");
