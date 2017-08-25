82ce95efbbf9a914d5290cd4ba18c160cb29e65f
godbiao <ahwangtingbiao@vip.qq.com>
2017-08-25 17:10:46 +0800

    tips

--- skins/js/common.js
+++ skins/js/common.js
@@ -501,52 +501,6 @@ function isMobile() {
 //小技巧
 function tips() {
 
-    var tipsdata = [
-        {
-            "id": 1,
-            "title": "还有这种操作？这个手势有点炫酷！",
-            "url": "http://mp.weixin.qq.com/s/55KNrkTMD3qEpYQD7zq9kg",
-            "img": "res/tips1.jpg",
-            "time": "2017-07-14"
-        }, {
-            "id": 2,
-            "title": "把emoji贴在键盘上！输入法键盘自定义可以玩一天！",
-            "url": "http://mp.weixin.qq.com/s/wRahzHyMvVOycjqU6KdaDA",
-            "img": "res/tips2.jpg",
-            "time": "2017-07-21"
-        }, {
-            "id": 3,
-            "title": "输入法还能当字典用？生僻字请给我来一沓！",
-            "url": "http://mp.weixin.qq.com/s/Eb6q76uljGPNReJ62rWMHg",
-            "img": "res/tips3.jpg",
-            "time": "2017-07-25"
-        }, {
-            "id": 4,
-            "title": "让前男友在输入法里消失，这个隐藏技巧终于来了！",
-            "url": "http://mp.weixin.qq.com/s/r2iBMlDwvCMJFIZsslrWVQ",
-            "img": "res/tips4.jpg",
-            "time": "2017-08-04"
-        }, {
-            "id": 5,
-            "title": "作女实战让输入更持久，1小时的持久怼就问你怕不怕！",
-            "url": "http://mp.weixin.qq.com/s/AA3l2XynO5CRThCkOuKiVw",
-            "img": "res/tips5.jpg",
-            "time": "2017-08-11"
-        }, {
-            "id": 6,
-            "title": "༺王者༻就应该这样取名！个性符号输入技巧速get！",
-            "url": "http://mp.weixin.qq.com/s/NJQNDP3kABDfuGsVk_nuUg",
-            "img": "res/tips6.jpg",
-            "time": "2017-08-18"
-        }, {
-            "id": 7,
-            "title": "实拍牛郎快速撩妹技能，看呆2亿单身狗……",
-            "url": "http://mp.weixin.qq.com/s/MtZ-ZUeniOe86TL6EN1Pwg",
-            "img": "res/tips7.jpg",
-            "time": "2017-08-25"
-        }
-        ];
-
     var randomnumber = Math.round(Math.random() * (tipsdata.length - 1));
 
     var newtips = tipsdata[randomnumber];
