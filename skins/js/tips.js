3caaebd86ca0b7b3e6a47bfe24ec5655bbdb009b
godbiao <ahwangtingbiao@vip.qq.com>
2017-09-18 09:42:37 +0800

    oloandtips

--- skins/js/tips.js
+++ skins/js/tips.js
@@ -1,71 +1,77 @@
 var tipsdata = [
     {
-        "id": 0,
+        "id": "0",
         "title": "IM追清风的微博",
         "url": "https://m.weibo.cn/u/1136590322",
         "img": "https://pic.kuaizhan.com/g2/M01/57/40/CgpQVFmnx_iAHFG-AABeIxE62182957803",
         "time": "2017-08-31",
         "isTips": "0"
         }, {
-        "id": 1,
+        "id": "1",
         "title": "还有这种操作？这个手势有点炫酷！",
         "url": "https://mp.weixin.qq.com/s/55KNrkTMD3qEpYQD7zq9kg",
-        "img": "https://pic.kuaizhan.com/g1/M01/66/D7/CgpQU1mnx5aASMNVAABE6JezvaE8389983",
+        "img": "https://pic.kuaizhan.com/g3/77/19/ca79-d501-41be-9fcf-28168ef3293e33",
         "time": "2017-07-14"
         }, {
-        "id": 2,
+        "id": "2",
         "title": "把emoji贴在键盘上！输入法键盘自定义可以玩一天！",
         "url": "https://mp.weixin.qq.com/s/wRahzHyMvVOycjqU6KdaDA",
-        "img": "https://pic.kuaizhan.com/g1/M00/66/D7/CgpQU1mnx5eAfTMPAABYTu5rFUU1008980",
+        "img": "https://pic.kuaizhan.com/g3/44/3b/6fa6-61f3-4727-bbc3-572c8606c1f101",
         "time": "2017-07-21"
         }, {
-        "id": 3,
+        "id": "3",
         "title": "输入法还能当字典用？生僻字请给我来一沓！",
         "url": "https://mp.weixin.qq.com/s/Eb6q76uljGPNReJ62rWMHg",
-        "img": "https://pic.kuaizhan.com/g1/M00/66/D7/CgpQU1mnx5eADsuTAABhFBxn5vI9310861",
+        "img": "https://pic.kuaizhan.com/g3/f1/d4/12eb-a766-49be-8861-27cb41fb168993",
         "time": "2017-07-25"
         }, {
-        "id": 4,
+        "id": "4",
         "title": "让前男友在输入法里消失，这个隐藏技巧终于来了！",
         "url": "https://mp.weixin.qq.com/s/r2iBMlDwvCMJFIZsslrWVQ",
-        "img": "https://pic.kuaizhan.com/g1/M00/66/D7/CgpQU1mnx5iAVVxKAABUXPL5PJc7860029",
+        "img": "https://pic.kuaizhan.com/g3/14/39/eb76-06f3-4f39-82ed-b346fa3c43fd74",
         "time": "2017-08-04"
         }, {
-        "id": 5,
+        "id": "5",
         "title": "作女实战让输入更持久，1小时的持久怼就问你怕不怕！",
         "url": "https://mp.weixin.qq.com/s/AA3l2XynO5CRThCkOuKiVw",
-        "img": "https://pic.kuaizhan.com/g1/M01/66/D7/CgpQU1mnx5iADO55AABsfjd420s3440542",
+        "img": "https://pic.kuaizhan.com/g3/f3/11/ed7e-c43d-4245-b778-1ce5cc84b91331",
         "time": "2017-08-11"
         }, {
-        "id": 6,
+        "id": "6",
         "title": "༺王者༻就应该这样取名！个性符号输入技巧速get！",
         "url": "https://mp.weixin.qq.com/s/NJQNDP3kABDfuGsVk_nuUg",
-        "img": "https://pic.kuaizhan.com/g1/M01/66/D7/CgpQU1mnx5mABgegAABXKNPYO-Q4327324",
+        "img": "https://pic.kuaizhan.com/g3/5c/bb/a014-6808-4801-8d27-52c25a5fe6d412",
         "time": "2017-08-18"
         }, {
-        "id": 7,
+        "id": "7",
         "title": "实拍牛郎快速撩妹技能，看呆2亿单身狗……",
         "url": "https://mp.weixin.qq.com/s/MtZ-ZUeniOe86TL6EN1Pwg",
-        "img": "https://pic.kuaizhan.com/g1/M01/66/D7/CgpQU1mnx5mAcj_9AAA9HIG5Jjw7015000",
+        "img": "https://pic.kuaizhan.com/g3/9e/7c/7095-cc55-4890-bcca-41939eeb1bea85",
         "time": "2017-08-25"
         }, {
-        "id": 8,
+        "id": "8",
         "title": "懒癌必备！拯救【(￢_￢)瞄Xiao#2】多合一字符串输入！",
         "url": "https://mp.weixin.qq.com/s/yjlmyMsz8w-JNsxYptPntQ",
-        "img": "https://pic.kuaizhan.com/g2/M00/5E/EB/CgpQVFmpD2OAG5SuAABchhCAXY41543195",
+        "img": "https://pic.kuaizhan.com/g3/b8/29/df2d-701d-4700-8d9d-207ed2fd274e88",
         "time": "2017-09-01"
         }, {
-        "id": 101,
+        "id": "09",
+        "title": "写出一个魑，把剩下的魅魍魉都召唤出来了！",
+        "url": "https://mp.weixin.qq.com/s/x_2xvZ1fN8--vTL1hdOR3A",
+        "img": "https://pic.kuaizhan.com/g3/6e/11/6092-23f9-4b0c-a533-d02ab22fc82b64",
+        "time": "2017-09-08"
+        }, {
+        "id": "10",
+        "title": "这些不为人知的输入法小功能，发现之后好用到爆！",
+        "url": "https://pic.kuaizhan.com/g3/90/70/0ffb-e0af-431b-94a0-cf8e725d3a1504",
+        "img": "res/tips9.jpg",
+        "time": "2017-09-15"
+        }, {
+        "id":"101",
         "title": "王者荣耀高清无损纯净击杀音效",
         "url": "https://pan.baidu.com/s/1eSmwCgi",
         "img": "https://pic.kuaizhan.com/g1/M00/81/E5/CgpQU1mszGKAeRSlAAAoO0p0U0o9416505",
         "time": "2017-09-04",
         "isTips": "0"
-        }, {
-        "id": "09",
-        "title": "写出一个魑，把剩下的魅魍魉都召唤出来了！",
-        "url": "https://mp.weixin.qq.com/s/x_2xvZ1fN8--vTL1hdOR3A",
-        "img": "https://pic.kuaizhan.com/g3/1a/42/dd47-f115-4ff7-90c9-5d92bf42b0c655",
-        "time": "2017-09-08"
         }
 ];
